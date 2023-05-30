01ec697d74a88564f6851167aa3c5a5d920eadc2
afiab <abidica30@stuy.edu>
2023-05-30 11:47:15 -0400

    uhh something with random isnt working

--- homework_creation/pixmaker.py
+++ homework_creation/pixmaker.py
@@ -1,5 +1,6 @@
 import random
 from PIL import Image
+import math
 
 def process_image(input, output):
     with Image.open(input) as image:
@@ -8,22 +9,24 @@ def process_image(input, output):
         for x in range(width):
             for y in range(height):
                 pixel = image.getpixel((x, y))
-                if pixel == (0, 0, 0): #black pixel
+                if pixel == (255, 255, 255): #white pixel
                     #make even red value
                     #for bit to end in 0
                     red = random.randint(0,254)
                     if red%2 == 1:
                         red+=1
-                    new_pixel = (red,
+                    #print("even,", red)
+                    new_pixel = (red,# 0, 0)
                                  random.randint(0, 255),
                                  random.randint(0, 254))
-                else: #white pixel
+                else: #black pixel
                     #make odd red value
                     #for bit to end in 1
                     red = random.randint(1,255)
                     if red%2 == 0:
                         red+=1
-                    new_pixel = (red,
+                    #print("odd", red)
+                    new_pixel = (red, #255, 255)
                                  random.randint(0, 255),
                                  random.randint(0, 255))
                 new_image.putpixel((x, y), new_pixel)
